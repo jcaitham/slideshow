7418af18b610e35f70a259c2ef48a5efb3b18099
jcaitham <jcaitham@gmail.com>
2023-02-24 18:27:20 -0600

    Update documentation

--- src/Slideshow.js
+++ src/Slideshow.js
@@ -1,8 +1,18 @@
 import React, { useEffect, useRef, useState } from 'react';
 import "./Slideshow.scss";
+/**
+ * Card that goes inside the main slideshow component.  You should flesh out the contents of the card.  Your contents should use width: 100%,
+ * rather than hard-coding any specific pixel widths
+ * @param props Special children property, plus any style overrides that you might like to apply to the card (such as border-radius)
+ */
 export const SlideshowCard = ({ children, style }) => {
     return (React.createElement("article", { className: "highlightWhite background card shadow", style: Object.assign({}, style) }, children));
 };
+/**
+ * A simple slideshow component.  Will show a horizontal list of cards along with scroll arrows to move left & right through the list.
+ * Will size itself to fill its container and will resize if the window resizes
+ * @param props The special children property, the desired width for each card, in pixels, plus any style overrides that you might like to apply to the list (like margins)
+ */
 const Slideshow = ({ children, cardWidth, style }) => {
     const data = useRef({ cardWidth: 0, totalWidth: 0, distancePerScroll: 0, windowWidth: 0 });
     const [contentRemainingToLeft, setContentRemainingToLeft] = useState(0);
@@ -14,25 +24,27 @@ const Slideshow = ({ children, cardWidth, style }) => {
         if (containerRef.current === null) {
             return;
         }
-        const windowWidth = containerRef.current.clientWidth;
-        //const cardWidth = 250;//Math.max(230, windowWidth / 4);
-        const totalWidth = children.length * (cardWidth + listGap);
+        const windowWidth = containerRef.current.clientWidth; // width of the "window" through which we are looking at the card list
+        const totalWidth = children.length * (cardWidth + listGap); // total width required by the list of cards
         setContentRemainingToRight(totalWidth - windowWidth);
         setContentRemainingToLeft(0);
         setScrollOffset(0);
         const distancePerScroll = totalWidth / children.length;
         data.current = { cardWidth, totalWidth, distancePerScroll, windowWidth };
     };
+    // on intial component mount, calculate some size-related things, and attach a handler to repeat this operation if the window resizes
     useEffect(() => {
         onMountAndResize();
         window.addEventListener("resize", onMountAndResize);
         return () => window.removeEventListener("resize", onMountAndResize);
     }, []);
+    // scroll to the right
     const incrementScroll = () => {
         setScrollOffset(Math.min(data.current.totalWidth - data.current.windowWidth, scrollOffset + data.current.distancePerScroll));
         setContentRemainingToRight(contentRemainingToRight - data.current.distancePerScroll);
         setContentRemainingToLeft(contentRemainingToLeft + data.current.distancePerScroll);
     };
+    // scroll to the left
     const decrementScroll = () => {
         setScrollOffset(Math.max(0, scrollOffset - data.current.distancePerScroll));
         setContentRemainingToLeft(contentRemainingToLeft - data.current.distancePerScroll);
--- src/Slideshow.tsx
+++ src/Slideshow.tsx
@@ -1,6 +1,12 @@
-import React, { ReactElement, ReactNode, useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import "./Slideshow.scss";
 
+
+/**
+ * Card that goes inside the main slideshow component.  You should flesh out the contents of the card.  Your contents should use width: 100%,
+ * rather than hard-coding any specific pixel widths
+ * @param props Special children property, plus any style overrides that you might like to apply to the card (such as border-radius)
+ */
 export const SlideshowCard = ({ children, style }: { children: React.ReactNode, style?: React.CSSProperties; }) =>
 {
 	return (
@@ -10,6 +16,11 @@ export const SlideshowCard = ({ children, style }: { children: React.ReactNode, 
 	);
 };
 
+/** 
+ * A simple slideshow component.  Will show a horizontal list of cards along with scroll arrows to move left & right through the list.  
+ * Will size itself to fill its container and will resize if the window resizes 
+ * @param props The special children property, the desired width for each card, in pixels, plus any style overrides that you might like to apply to the list (like margins)
+ */
 const Slideshow = ({ children, cardWidth, style }: { children: React.ReactNode[], cardWidth: number, style?: React.CSSProperties; }) =>
 {
 	const data = useRef({ cardWidth: 0, totalWidth: 0, distancePerScroll: 0, windowWidth: 0 });
@@ -29,11 +40,9 @@ const Slideshow = ({ children, cardWidth, style }: { children: React.ReactNode[]
 		{
 			return;
 		}
-		const windowWidth = containerRef.current.clientWidth;
+		const windowWidth = containerRef.current.clientWidth;  // width of the "window" through which we are looking at the card list
 
-		//const cardWidth = 250;//Math.max(230, windowWidth / 4);
-
-		const totalWidth = children.length * (cardWidth + listGap);
+		const totalWidth = children.length * (cardWidth + listGap);  // total width required by the list of cards
 
 		setContentRemainingToRight(totalWidth - windowWidth);
 		setContentRemainingToLeft(0);
@@ -43,7 +52,7 @@ const Slideshow = ({ children, cardWidth, style }: { children: React.ReactNode[]
 		data.current = { cardWidth, totalWidth, distancePerScroll, windowWidth };
 	};
 
-
+	// on intial component mount, calculate some size-related things, and attach a handler to repeat this operation if the window resizes
 	useEffect(() =>
 	{
 		onMountAndResize();
@@ -53,7 +62,7 @@ const Slideshow = ({ children, cardWidth, style }: { children: React.ReactNode[]
 		return () => window.removeEventListener("resize", onMountAndResize);
 	}, []);
 
-
+	// scroll to the right
 	const incrementScroll = () =>
 	{
 		setScrollOffset(Math.min(data.current.totalWidth - data.current.windowWidth, scrollOffset + data.current.distancePerScroll));
@@ -61,6 +70,7 @@ const Slideshow = ({ children, cardWidth, style }: { children: React.ReactNode[]
 		setContentRemainingToLeft(contentRemainingToLeft + data.current.distancePerScroll);
 	};
 
+	// scroll to the left
 	const decrementScroll = () =>
 	{
 		setScrollOffset(Math.max(0, scrollOffset - data.current.distancePerScroll));
@@ -79,9 +89,7 @@ const Slideshow = ({ children, cardWidth, style }: { children: React.ReactNode[]
 							style: { ...child.props.style, width: data.current.cardWidth + "px" }
 						});
 					}
-
 				})}
-				{/* {contents.map((content, index) => <SlideshowCard key={index} style={{ width: data.current.cardWidth + "px" }}>{content}</SlideshowCard>)} */}
 			</div>
 			<div className={"floatingArrow left" + (contentRemainingToLeft <= 0 ? " disabled" : "")} onClick={decrementScroll}><div className="arrow left"></div></div>
 			<div className={"floatingArrow right" + (contentRemainingToRight <= 0 ? " disabled" : "")} onClick={incrementScroll}><div className="arrow right"></div></div>
